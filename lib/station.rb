c50fcbeeb1d3ba730dbff97a67b671f0943642c5
marcusfgardiner <marcusfgardiner@gmail.com>
2018-01-31 17:14:24 +0000

    Station working as a constant, need it to be an instance variable lol

--- lib/station.rb
+++ lib/station.rb
@@ -1,9 +1,8 @@
 class Station
-
   STATIONS_LIST = [
-      bakerpoo: 1,
-      pickawilly: 2,
-      boobily: 3
-  ]
+    { bakerpoo: 1 },
+    { pickawilly: 2 },
+    { boobily: 3 }
+  ].freeze
 
 end
